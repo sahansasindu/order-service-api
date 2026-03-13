cffdb30584724f9f3eb01af18a13dba8299e4ce1
Sahan Sasindu <119693671+sahansasindu@users.noreply.github.com>
2026-03-13 14:28:17 +0530

    update

--- src/main/java/com/dev/quickcart/order_service_api/dto/response/paginate/CustomerOrderPaginateDto.java
+++ src/main/java/com/dev/quickcart/order_service_api/dto/response/paginate/CustomerOrderPaginateDto.java
@@ -1,12 +1,17 @@
 package com.dev.quickcart.order_service_api.dto.response.paginate;
 
 import com.dev.quickcart.order_service_api.dto.response.CustomerOrderResponseDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
-
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerOrderPaginateDto {
     private long count;
     private List<CustomerOrderResponseDto>dataList;
